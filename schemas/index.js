7f0b4587a528a65a987d514649b5e699956c7656
karmalexz <hellozhangdi1994@gmail.com>
2023-02-21 23:54:49 +1100

    created schemas

---
+++ schemas/index.js
@@ -0,0 +1,7 @@
+import host from './host'
+import person from './person'
+import property from './property'
+import propertyImage from './propertyImage'
+import review from './review'
+import traveller from './traveller'
+export const schemaTypes = [property, propertyImage, review, person, traveller, host]
